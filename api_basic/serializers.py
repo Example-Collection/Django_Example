dcaf2c2d115d2b0ee526e94f6ccd3b55b6921b58
sangwoo-98 <robbyra@gmail.com>
2021-03-24 15:56:29 +0900

    Create comments about how to use serializer

--- api_basic/serializers.py
+++ api_basic/serializers.py
@@ -17,3 +17,25 @@ class ArticleSerializer(serializers.Serializer):
         # save() : Saves instance to database.
         instance.save()
         return instance
+
+    # How to use Serializer
+
+    # 1. create new Article
+    # article = Article(title = "sample title", author = "sample author", email = "sample@email.com")
+
+    # 2. save new Article instance
+    # article.save()
+
+    # 3. Create new ArticleSerializer instance.
+    # serializer = new ArticleSerializer(article)
+
+    # 4. Read data as JSON format using `serializer.data` .
+    # Output : {'title': 'sample title', 'author': 'sample author', email: 'sample@email.com'}
+
+    # 5. Render data as JSON format.
+    # content = JSONRenderer().render(serializer.data);
+    # JSONRenderer above is imported from `rest_framework.renderers`.
+
+    # 6. To render more than 1 objects
+    # Use the code below.
+    # serializer = ArticleSerializer(Article.objects.all(), many=True)
