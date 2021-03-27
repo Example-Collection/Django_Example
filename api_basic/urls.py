7d7e962122654809f7d4231a12d8d8eb3e0ad7c1
sangwoo-98 <robbyra@gmail.com>
2021-03-27 16:58:39 +0900

    Using GenericApiView and mixins

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import article_list, article_detail, ArticleAPIView, ArticleDetailAPIView, GenericAPIView
 
 urlpatterns = [
-    path('article/', GenericAPIView.as_view()),
+    path('article/<int:id>/', GenericAPIView.as_view()),
     # path('article/', article_list),
     # path('article/', ArticleAPIView.as_view()),
     # path('detail/<int:pk>', article_detail)
-    path('detail/<int:id>/', ArticleDetailAPIView.as_view())
+    # path('detail/<int:id>/', ArticleDetailAPIView.as_view())
 ]
--- api_basic/views.py
+++ api_basic/views.py
@@ -11,15 +11,27 @@ from rest_framework import mixins
 
 
 # Generic Views and Mixins
-class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin,
+                     mixins.CreateModelMixin, mixins.UpdateModelMixin,
+                     mixins.RetrieveModelMixin, mixins.DestroyModelMixin):
     serializer_class = ArticleModelSerializer
     queryset = Article.objects.all()
+    lookup_field = 'id'
 
-    def get(self, request):
-        return self.list(request)
+    def get(self, request, id=None):
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
 
     def post(self, request):
         return self.create(request)
+
+    def put(self, request, id=None):
+        return self.update(request, id)
+
+    def delete(self, request, id=None):
+        return self.destroy(request, id)
 
 
 # Class based API Views
